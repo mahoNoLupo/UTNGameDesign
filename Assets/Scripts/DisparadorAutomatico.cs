31f4583a4fa7095654847432bc91c2c84d80526e
lena luppo <mahonolena@gmail.com>
2022-08-04 22:18:29 -0300

    Object interaction
    -DisparadorAutomatico added.
    -Disparador refactored.
    -Bomba changes.
    -GeneradorOleadas added.
    -DestructorTemporizado added.

--- Assets/Scripts/DisparadorAutomatico.cs
+++ Assets/Scripts/DisparadorAutomatico.cs
@@ -6,15 +6,24 @@ public class DisparadorAutomatico : MonoBehaviour
 {
     [SerializeField]
     private GameObject prefab;
+    [SerializeField]
+    private float timeToInvoke;
+    [SerializeField]
+    private float repeatTime;
 
     private void Awake()
     {
-        Instantiate(prefab, transform.position, transform.rotation);
+        InvokeRepeating("Disparar", timeToInvoke, repeatTime);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
+    void Disparar()
+    {
+        Instantiate(prefab, transform.position, transform.rotation);
+    }
+
 }
